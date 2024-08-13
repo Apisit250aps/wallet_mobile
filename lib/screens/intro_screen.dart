3ed5a533ce00d1db935963f1856859e1d9c5b8f8
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-13 22:19:07 +0700

    image

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/wallet_transparent.png",
+              width: 250,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
